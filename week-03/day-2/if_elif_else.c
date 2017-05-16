a3eef90c2f00996a92313e1f6a58794d28a2c6d4
tamasak <tamas.akos@gmail.com>
2017-05-16 12:56:12 +0200

    week3 day2 tasks part1

---
+++ week-03/day-2/if_elif_else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the TOTORO macro which holds the number of students in our class
+#define TOTORO 19
+
+int main()
+{
+    // Use the #ifdef and #ifndef macros!
+
+    // If the TOTORO macro is not defined print out 32
+    #ifndef TOTORO
+        printf("32");
+    #endif
+
+    // If the TOTORO macro is defined print out it's value
+    #ifdef TOTORO
+        printf("The number of students is: %d",TOTORO);
+    #endif
+
+    return 0;
+}
