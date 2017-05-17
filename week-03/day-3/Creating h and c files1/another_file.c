08f75cd5003c97d0065914597af0db4f04c87c4e
tamasak <tamas.akos@gmail.com>
2017-05-17 14:17:42 +0200

    week3 day3 tasks part1

---
+++ week-03/day-3/Creating h and c files1/another_file.c
@@ -0,0 +1,17 @@
+
+// TODO: Include needed header files
+#include <stdint.h>
+#include "another_file.h"
+
+
+void StringManipulator(char* str){
+    uint32_t str_len = strlen(str);
+
+    for(uint32_t i = 0; i < str_len; i++) {
+        str[i] -= 1;
+    }
+}
+
+void IntegerManipulator(int* num){
+    *num = *num - an_example_number;
+}
