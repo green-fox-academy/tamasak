bc1886eadf76ee7a87e860553b76c7c1151bd771
tamasak <tamas.akos@gmail.com>
2017-05-24 17:22:52 +0200

    week 4 day 3 tasks part4

--- week-04/day-3/dynamically allocated memory/09_student_subject_grade/main.c
+++ week-04/day-3/dynamically allocated memory/09_student_subject_grade/main.c
@@ -21,6 +21,17 @@ typedef struct {
 // It should take it's name and it's age as parameter
 // It should allocate 5 grades
 // It should set the grade_count to 0
+student_t *create_student(char *name, int age)
+{
+    grade_t *grades = (grade_t*) calloc(5, sizeof(grade_t));
+    student_t *student = (student_t*) malloc(sizeof(student_t));
+    strcpy(student->name, name);
+    student->age = age;
+    student->grades = grades;
+    student->grade_count = 0;
+
+    return student;
+}
 
 // TODO:
 // Create a function that takes 3 parameters:
@@ -28,18 +39,48 @@ typedef struct {
 // A Subject
 // An int (1-5)
 // It should set the last grade's values with the given arguments
+void set_grade(student_t *student, subject_t subject, int a)
+{
+    student->grades[student->grade_count].subject = subject;
+    student->grades[student->grade_count].value = a;
+    student->grade_count++;
+}
 
 // TODO:
 // Create a function that takes a Student as a pointer and returns the subject
 // that the student has worst grade in
+subject_t worst_grade(student_t *student)
+{
+    int tmp = 100;
+    int tmp2 = 50;
+    //for (int j = 0; j <)
+    for (int i = 0; i < student->grade_count; i++) {
+        if (student->grades[i].value < tmp) {
+            tmp = student->grades[i].value;
+            tmp2 = student->grades[i].subject;
+        }
+    }
+    return tmp2;
+}
 
 // TODO:
 // Create a function that deconstructs a Student
 // It should take a pointer that points to the student
 // It should free each Grade and the Student itself
+void deconstruct_student(student_t *student)
+{
+    free(student->grades);
+    free(student);
+}
 
 int main()
 {
+    student_t *pisti = create_student("Pisti", 8);
+    set_grade(pisti, HISTORY, 3);
+    set_grade(pisti, ENGLISH, 1);
+    set_grade(pisti, SCIENCE, 3);
+
+printf("%s's worst grade subject is: %d\n", pisti->name, worst_grade(pisti));
 
     return 0;
 }
