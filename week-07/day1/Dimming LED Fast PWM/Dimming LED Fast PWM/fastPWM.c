b1094958fc89bdddd2a82295e71657ab3f897e69
tamasak <tamas.akos@gmail.com>
2017-06-12 11:26:04 +0200

    week7 day1 tasks p2

--- week-07/day1/Dimming LED Fast PWM/Dimming LED Fast PWM/fastPWM.c
+++ week-07/day1/Dimming LED Fast PWM/Dimming LED Fast PWM/fastPWM.c
@@ -7,11 +7,14 @@ void timer0_init(void)
 {
 	TCCR0A |= 1 << COM0A1; //non-inverting mode
 	TCCR0A |= 1 << WGM00;
-	TCCR0A |= 1 << WGM01;
-	//TCCR0B |= 1 << CS00;
-	//TCCR0B |= 1 << CS02; // prescaler
+	TCCR0A |= 1 << WGM01; // fast pwm mode
+	TCCR0B |= 1 << CS00;
+	/*TCCR0B |= 1 << CS02; // prescaler
+	TIFR0 |= OCF0A; //Timer/Counter0, Output Compare A Match Flag
+	TIFR0 |= TOV0; //  Timer/Counter0, Overflow Flag
+	TIMSK0 |= 1 << OCIE0A; //Timer/Counter0, Output Compare A Match Interrupt Enable
 	TIMSK0 |= 1 << TOIE0; // Enable the TC0 overflow interrupt
-	sei();	// Enable the interrupts globally
+	sei();	// Enable the interrupts globally*/
 }
 
 void set_duty_cycle(uint8_t duty)
@@ -25,5 +28,4 @@ void set_duty_cycle(uint8_t duty)
 void init_led(void)
 {
 	DDRD |= 1 << DDRD6;
-	PORTD |= 1 << PORTD6;
 }
