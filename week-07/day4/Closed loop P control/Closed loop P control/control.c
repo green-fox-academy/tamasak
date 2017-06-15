6b5b3b72a51b578065003eb85393fc0cabc8b01d
tamasak <tamas.akos@gmail.com>
2017-06-15 13:10:42 +0200

    week7 day4 tasks p2

--- week-07/day4/Closed loop P control/Closed loop P control/control.c
+++ week-07/day4/Closed loop P control/Closed loop P control/control.c
@@ -8,5 +8,5 @@ int16_t p_control(uint16_t rpm, float p_value)
 {
 	float error = (float)rpm - (ADC_Read() * 5);
 	int16_t signal_value = error * p_value;
-	return = signal_value;
+	return signal_value;
 }
---
+++ week-07/day4/Closed loop P control/Closed loop P control/fastPWM.c
@@ -0,0 +1,35 @@
+﻿#include <avr/io.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+#include "fastPWM.h"
+
+void timer2_init(void)
+{
+	TCCR2A |= 1 << COM2A1; //non-inverting mode
+	TCCR2A |= 1 << WGM20;
+	TCCR2A |= 1 << WGM21; // fast pwm mode
+	TCCR2B |= 1 << CS20;
+	DDRB |= 1 << DDRB3;
+	//PORTB |= 1 << PORTB3;
+	/*TCCR0B |= 1 << CS02; // prescaler
+	TIFR0 |= OCF0A; //Timer/Counter0, Output Compare A Match Flag
+	TIFR0 |= TOV0; //  Timer/Counter0, Overflow Flag
+	TIMSK0 |= 1 << OCIE0A; //Timer/Counter0, Output Compare A Match Interrupt Enable
+	TIMSK0 |= 1 << TOIE0; // Enable the TC0 overflow interrupt
+	sei();	// Enable the interrupts globally*/
+}
+
+void set_duty_cycle(uint8_t duty)
+{
+	if (duty > 255)
+		duty = 255;
+	else if (duty < 0)
+		duty = 0;
+	OCR2A = duty;
+	//printf("%d\n", duty);
+}
+/*void init_led(void)
+{
+	DDRB |= 1 << DDRB3;
+	//PORTD |= 1 << PORTD6;
+}*/
